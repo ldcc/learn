1554bd6b89437d6ab7d2e167b8afa883c73b107e
ldcc <ldcc@live.com>
2019-07-31 20:04:54 +0800

    save interp

--- haskell/algos/app/Main.hs
+++ haskell/algos/app/Main.hs
@@ -1,13 +1,6 @@
 module Main where
 
---import SimpleInteractiveInterpreter
---
---main :: IO ()
---main = putStrLn . show $ pass1 "fn avg x y => (x + y) / 2"
-
-
-
-import TinyThreePassCompiler
+import SimpleInteractiveInterpreter
 
 main :: IO ()
-main = putStrLn . show $ simulate (compile "[ x y z ] ( 2*3*x + 5*y - 3*z ) / (1 + 3 + 2*2)") [4, 0, 0]
+main = putStrLn . show $ input "fn avg x y => (x + y) / 2" newInterpreter -- (fn avg x y => e) ::= (avg = fn x y -> e)
--- haskell/algos/src/SimpleInteractiveInterpreter.hs
+++ haskell/algos/src/SimpleInteractiveInterpreter.hs
@@ -3,37 +3,25 @@ module SimpleInteractiveInterpreter where
 import Text.Read (readMaybe)
 import Data.List (elemIndex)
 import Data.Maybe (isJust, fromJust)
+import Data.Map as Map (Map, fromList, lookup, insert, (!), member)
 
---- Interpreter which represented the REPL environment
 type Result = Maybe Double
-data Interpreter
-data Arith = Plus | Minus | Times | Divide deriving (Show, Read)
+type Interpreter = Map String Ast
 data Ast = Const Double
          | Symbol String
-         | Proc Arith Ast Ast
          | Invoke String [Ast]
-         | Closure String [Ast] Ast deriving (Show, Read)
+         | Closure [Ast] Ast deriving (Show, Read)
 
 newInterpreter :: Interpreter
-newInterpreter = undefined
+newInterpreter = fromList $ [("+", makeArith "+"), ("-", makeArith "-"), ("*", makeArith "*"), ("/", makeArith "/")]
+  where makeArith s = Closure [Symbol "x", Symbol "y"] $ Invoke s [Symbol "x", Symbol "y"]
 
-input :: String -> Interpreter -> Either String (Result, Interpreter)
-input _ _ = Left "Not implemented"
+input :: String -> Interpreter -> Either String [String]
+input prog interp = Right (parse prog) >>= genAst interp
 
-pass1 :: String -> [String]
-pass1 = reverse . words . foldl (\ acc t -> acc ++ if elem t "+-*/()" then [' ', t, ' '] else [t]) []
+parse :: String -> [String]
+parse = parsing [] [] . reverse . words . Prelude.foldl (\ acc t -> acc ++ if elem t "+-*/()" then [' ', t, ' '] else [t]) []
   where
---    generates (t:ts0)
---      | elem t ["+", "-", "*", "/"] = let
---          (car, ts1) = generates ts0
---          (cdr, ts2) = generates ts1
---        in case t of
---          "+" -> (Add car cdr, ts2)
---          "-" -> (Sub car cdr, ts2)
---          "*" -> (Mul car cdr, ts2)
---          "/" -> (Div car cdr, ts2)
---      | isJust $ (readMaybe t :: Maybe Int) = (Imm $ read t, ts0)
---      | otherwise = (Arg $ fromJust $ flip elemIndex params t, ts0)
     parsing stack1 stack2 [] = merging stack1 stack2 [] $ \_ -> True
     parsing stack1 stack2 (t:ts)
       | t == ")" = parsing (t:stack1) stack2 ts
@@ -51,3 +39,24 @@ pass1 = reverse . words . foldl (\ acc t -> acc ++ if elem t "+-*/()" then [' ',
       | p s = merging ss (s : stack2) tokens p
       | head tokens == "(" = parsing ss stack2 (tail tokens)
       | otherwise = parsing (head tokens : s : ss) stack2 (tail tokens)
+
+genAst :: Interpreter -> [String] -> Either String [String]
+genAst interp ("fn":name:ts) = case Map.lookup name interp of
+  Just (Const _) -> Left "Conflicts Error!"
+  _ -> genAst interp $ name : "=" : "fn" : ts
+genAst interp (t:ts0) = Right $ t : ts0
+
+--  | member interp t =
+--  | elem t ["+", "-", "*", "/"] = let
+--      (car, ts1) = generates ts0
+--      (cdr, ts2) = generates ts1
+--    in case t of
+--      "+" -> (Add car cdr, ts2)
+--      "-" -> (Sub car cdr, ts2)
+--      "*" -> (Mul car cdr, ts2)
+--      "/" -> (Div car cdr, ts2)
+--  | isJust $ (readMaybe t :: Maybe Int) = (Imm $ read t, ts0)
+--  | otherwise = (Arg $ fromJust $ flip elemIndex params t, ts0)
+
+--closure
+
