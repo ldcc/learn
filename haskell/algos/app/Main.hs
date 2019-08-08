67bc8f21bd5c430f582b16f22f098312b25b3ff8
ldcc <ldcc@live.com>
2019-08-08 11:16:48 +0800

    save hs interp

--- haskell/algos/app/Main.hs
+++ haskell/algos/app/Main.hs
@@ -4,4 +4,4 @@ import Data.Either (fromRight)
 import SimpleInteractiveInterpreter
 
 main :: IO ()
-main = putStrLn . show $ input "x" newInterpreter >>= return . fst
+main = putStrLn . show $ input "x = 2" newInterpreter >>= return . fst
