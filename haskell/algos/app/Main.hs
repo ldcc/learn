f474485e448f4bf38fee8a05c3a621bdab1a84d4
ldcc <ldcc@live.com>
2019-09-22 10:40:18 +0800

    hs potatoes

--- haskell/algos/app/Main.hs
+++ haskell/algos/app/Main.hs
@@ -1,15 +1,9 @@
 module Main where
 
-import Data.Either (fromRight)
-import SimpleInteractiveInterpreter
+import Potatoes
 
 main :: IO ()
---main = putStrLn . show $ input "" newInterpreter
 main = do
-  let int0 = newInterpreter
-  let ret1 = input "x = 1" int0
-  putStrLn . show $ fst <$> ret1
-  let ret2 = ret1 >>= input "fn add x y => x + z" . snd
-  putStrLn $ show $ fst <$> ret2
-  let ret3 = ret1 >>= input "fn add x x => x + x" . snd
-  putStrLn $ show $ fst <$> ret3
+  putStrLn . show $ potatoes 99 100 98
+  putStrLn . show $ potatoes 84 65 80
+  putStrLn . show $ potatoes 75 96 70
---
+++ haskell/algos/src/Potatoes.hs
@@ -0,0 +1,4 @@
+module Potatoes where
+
+--potatoes :: Int -> Int -> Int -> Int
+potatoes p0 w0 p1 = w0 * (100 - p0) `div` (100 - p1)
