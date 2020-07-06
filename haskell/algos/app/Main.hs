10ddb364b521a6ac97781b9638b46181921ba618
Ldc <ldcc@live.com>
2020-07-06 10:11:31 +0800

    archive parseRegExp

--- haskell/algos/app/Main.hs
+++ haskell/algos/app/Main.hs
@@ -4,4 +4,8 @@ import RegExpParser
 
 main :: IO ()
 main = do
-  putStrLn . show $ parseRegExp "|*"
+  putStrLn . show $ parseRegExp "a*a"
+  putStrLn . show $ parseRegExp "(a)*a"
+  putStrLn . show $ parseRegExp "*a"
+  putStrLn . show $ parseRegExp "*aa"
+  putStrLn . show $ parseRegExp "**a"
--- haskell/algos/src/RegExpParser.hs
+++ haskell/algos/src/RegExpParser.hs
@@ -10,6 +10,7 @@ data RegExp = Normal Char       -- ^ A character that is not in "()*|."
 
 parseRegExp :: String -> Maybe RegExp
 parseRegExp [] = Nothing
+parseRegExp ['*'] = Nothing
 parseRegExp token = splitExp 0 token >>= \ (r, l) -> case r of
   [] -> parsing l
   _ -> Or <$> parseRegExp l <*> parseRegExp r
@@ -18,10 +19,12 @@ parsing :: String -> Maybe RegExp
 parsing [] = Just None
 parsing ['.'] = Just Any
 parsing ['*'] = Just $ ZeroOrMore None
-parsing [char] = Just $ Normal char
-parsing (t:ts) = ret >>= \ (r, l) -> match <$> parseRegExp l <*> parsing r >>= id
-  where ret | t == '(' = pickExp 0 (t:ts)
-            | otherwise = Just (ts, [t])
+parsing [token] = Just $ Normal token
+parsing (t:ts) = ret >>= \ _ret -> case _ret of
+  (('*':r), l) -> patgen l r ZeroOrMore
+  (r, l) -> patgen l r id
+  where ret = if t == '(' then pickExp 0 (t:ts) else Just (ts, [t])
+        patgen l r f = match <$> (f <$> parseRegExp l) <*> parsing r >>= id
 
 match e1 None = Just e1
 match e1 (Str exps) = Just . Str $ e1 : exps
@@ -30,6 +33,7 @@ match e1 (ZeroOrMore None) = Just $ ZeroOrMore e1
 match e1 e2 = Just $ Str [e1, e2]
 
 splitExp :: Int -> String -> Maybe (String, String)
+splitExp _ ('|':[]) = Nothing
 splitExp 0 ('|':ts) = Just (ts, [])
 splitExp 0 [] = Just ([], [])
 splitExp (-1) _ = Nothing
