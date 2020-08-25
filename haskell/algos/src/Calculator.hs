31a6c0386da0c8038614ec0f9589300ce0643f3f
Ldc <ldcc@live.com>
2020-08-25 12:14:06 +0800

    optimize calc

--- haskell/algos/src/Calculator.hs
+++ haskell/algos/src/Calculator.hs
@@ -6,21 +6,20 @@ data Arith = Plus | Minus | Times | Divide deriving (Show, Read)
 evaluate :: String -> Double
 evaluate = calc . fst . pass . parse . reverse . words
 
+calc :: Ast -> Double
+calc (Proc op l r) = (pickop op) (calc l) (calc r)
+calc (V v) = v
+
 pass :: [String] -> (Ast, [String])
-pass [p] = (V (read p), [])
 pass (p:ps0)
-  | isp p = (Proc Plus car cdr, ps2)
-  | iss p = (Proc Minus car cdr, ps2)
-  | ism p = (Proc Times car cdr, ps2)
-  | isd p = (Proc Divide car cdr, ps2)
+  | p == "+" = (Proc Plus car cdr, ps2)
+  | p == "-" = (Proc Minus car cdr, ps2)
+  | p == "*" = (Proc Times car cdr, ps2)
+  | p == "/" = (Proc Divide car cdr, ps2)
   | otherwise = (V (read p), ps0)
   where
     (car, ps1) = pass ps0
     (cdr, ps2) = pass ps1
-
-calc :: Ast -> Double
-calc (Proc op l r) = (pickop op) (calc l) (calc r)
-calc (V v) = v
 
 parse :: [String] -> [String]
 parse = parsing [] []
@@ -28,20 +27,15 @@ parse = parsing [] []
   parsing :: [String] -> [String] -> [String] -> [String]
   parsing stack1 stack2 [] = merging stack1 stack2 [] (\_ -> True)
   parsing stack1 stack2 (t:ts)
-    | any ($ t) [ism, isd] = parsing (t:stack1) stack2 ts
-    | any ($ t) [isp, iss] = merging stack1 stack2 (t:ts) id
+    | elem t ["*", "/"] = parsing (t:stack1) stack2 ts
+    | elem t ["+", "-"] = merging stack1 stack2 (t:ts) $ flip elem ["*", "/"]
     | otherwise = parsing stack1 (t:stack2) ts
   merging :: [String] -> [String] -> [String] -> (Bool -> Bool) -> [String]
   merging [] stack2 [] _ = stack2
   merging [] stack2 (t:ts) _ = parsing [t] stack2 ts
-  merging (s:ss) stack2 tokens p
-    | any (p . ($ s)) [ism, isd] = merging ss (s : stack2) tokens p
-    | otherwise = parsing (head tokens : s : ss) stack2 (tail tokens)
-
-isp = (== "+")
-iss = (== "-")
-ism = (== "*")
-isd = (== "/")
+  merging (s:ss) stack2 tokens@(t:ts) p
+    | p s = merging ss (s : stack2) tokens p
+    | otherwise = parsing (t : s : ss) stack2 ts
 
 pickop Plus = (+)
 pickop Minus = (-)
