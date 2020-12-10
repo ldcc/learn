3ddbe2aef3bee3ba61a5d5508d5a31b3d05d918f
Ldc <ldcc@live.com>
2020-12-10 16:43:27 +0800

    save bak

--- python/pp/mangood.py
+++ python/pp/mangood.py
@@ -33,7 +33,7 @@ def gps_crdi(name, data):
         for j in range(len(i) - 1):
             b += i[j] + '.*'
         b += i[-1]
-        for i in data['产品组合'].values:
+        for i in data:
             i_new = i.replace(',', '')
             res = re.search(b, i_new)
             if not (res is None):
@@ -44,7 +44,7 @@ def gps_crdi(name, data):
 def calc(data):
     r_1_di = de(int)
 
-    for i in data['产品组合'].values:
+    for i in data:
         for j in set(i.split(',')):
             r_1_di[j] += 1  # 统计各个词的频次
     di = gps_de(r_1_di, 2)  # 限制最小频数
@@ -76,8 +76,7 @@ def groupby(rows):
         if goods is None:
             goods = [component]
         else:
-            goods = goods.append(component)
-        print(component, goods, goods is None)
+            goods.append(component)
         orders[guest] = goods
     return orders
 
@@ -86,10 +85,13 @@ dataset = pandas.read_excel(r'dataset220201209.xlsx', sheet_name='业绩总体')
 
 if __name__ == '__main__':
     orders = groupby(dataset.iterrows())
-    dataset = pandas.DataFrame([orders]).T.reset_index().rename(columns={'index': '客户', 0: '产品组合'})
-    print(dataset)
-    # di_3_set = []
-    # for _, row in dataset.iterrows():
-    #     print(row['产品组合'])
-    #     di_3_set = di_3_set.append(calc(row))
-    # print(di_3_set)
+    print(orders)
+    di_3s = []
+    for _, order in orders.items():
+        print('--------------------------------------------')
+        print(order)
+        di_3 = calc(order)
+        if di_3 is not None:
+            di_3s.append(di_3)
+        print('--------------------------------------------')
+    print(di_3s)
