80a6778c0d6e091268b343a2573a7690ee289b24
Ldc <ldcc@live.com>
2020-12-11 14:28:05 +0800

    solve

--- python/pp/mangood.py
+++ python/pp/mangood.py
@@ -1,6 +1,5 @@
 import re
 import pandas
-from collections import defaultdict as de
 
 
 def dell(d, na):
@@ -17,17 +16,18 @@ def gps_de(d, n):
     return dell(d, name)
 
 
-def gps_cr(d):
-    name = []
-    for k, v in d.items():
-        name.append(k)
-    name_new = [i + j[-1] for i in name for j in name if i[1:] == j[:-1]]
+def gps_cr(d, first):
+    name = d.keys()
+    if first:
+        name_new = [i + j[-1] for i in name for j in name]
+    else:
+        name_new = [i + j[-1] for i in name for j in name if i[1:] == j[:-1]]
     # gsp理论，
     return name_new
 
 
 def gps_crdi(name, data):
-    r = de(int)
+    r = {}
     for i in name:
         b = ''
         for j in range(len(i) - 1):
@@ -37,19 +37,47 @@ def gps_crdi(name, data):
             i_new = i.replace(',', '')
             res = re.search(b, i_new)
             if not (res is None):
-                r[b.replace('.*', '')] += 1
+                v = b.replace('.*', '')
+                if r.get(v) is None:
+                    r[v] = 0
+                r[v] += 1
     return r
 
 
-def calc(data):
-    r_1_di = de(int)
-
+def gps2(data):
+    r_1_di = {}
     for i in data:
         for j in set(i.split(',')):
-            r_1_di[j] += 1  # 统计各个词的频次
+            if r_1_di.get(j) is None:
+                r_1_di[j] = 0
+            r_1_di[j] += 1
+
     di = gps_de(r_1_di, 2)  # 限制最小频数
-    print('L1:')
-    print(di)
+    nuu = 1
+    ret = {}
+    while True:
+        print('L' + str(nuu) + ':', di)
+        name_n = gps_cr(di, nuu == 1)
+        print(name_n)
+        r_di = gps_crdi(name_n, data)
+        di = gps_de(r_di, 2)
+        if len(di) == 0:
+            break
+        ret.update(di)
+        nuu += 1
+    return ret
+
+
+def gps(data):
+    r_1_di = {}
+    for i in data:
+        for j in set(i.split(',')):
+            if r_1_di.get(j) is None:
+                r_1_di[j] = 0
+            r_1_di[j] += 1  # 统计各个词的频次
+
+    di = gps_de(r_1_di, 2)  # 限制最小频数
+    print('L1:', di)
     name = []
     for k, v in di.items():
         name.append(k)  # 取出需要的词
@@ -57,14 +85,15 @@ def calc(data):
     r_di = gps_crdi(name_new, data)  # 计算各个自由组合出现的频次
     di_3 = gps_de(r_di, 2)  # 将取出来的组合再次用频次限制
     nuu = 1
+    ret = {}
     while len(di_3) != 0:
-        print('L' + str(nuu + 1) + ':')
-        print(di_3)
-        name_n = gps_cr(di_3)
+        print('L' + str(nuu + 1) + ':', di_3)
+        name_n = gps_cr(di_3, False)
         r_di = gps_crdi(name_n, data)
         di_3 = gps_de(r_di, 2)
         nuu += 1
-    return di_3
+        ret.update(di)
+    return ret
 
 
 def groupby(rows):
@@ -81,17 +110,23 @@ def groupby(rows):
     return orders
 
 
-dataset = pandas.read_excel(r'dataset220201209.xlsx', sheet_name='业绩总体')
-
-if __name__ == '__main__':
+def calc():
     orders = groupby(dataset.iterrows())
+    di_3s = {}
     print(orders)
-    di_3s = []
-    for _, order in orders.items():
+    for k, order in orders.items():
         print('--------------------------------------------')
         print(order)
-        di_3 = calc(order)
-        if di_3 is not None:
-            di_3s.append(di_3)
+        di_3 = gps(order)
+        di_3p = gps2(order)
+        print(di_3 == di_3p)
+        if di_3 is not None and len(di_3) != 0:
+            di_3s[k] = di_3
         print('--------------------------------------------')
-    print(di_3s)
+    return di_3s
+
+
+dataset = pandas.read_excel(r'dataset220201209.xlsx', sheet_name='业绩总体')
+if __name__ == '__main__':
+    gpsde = calc()
+    print(gpsde)
