b3292f412d1377baca3f45aab46a338d61039986
Ldc <ldcc@live.com>
2020-12-11 14:35:02 +0800

    solve

--- python/pp/mangood.py
+++ python/pp/mangood.py
@@ -19,10 +19,9 @@ def gps_de(d, n):
 def gps_cr(d, first):
     name = d.keys()
     if first:
-        name_new = [i + j[-1] for i in name for j in name]
+        name_new = [i + j for i in name for j in name]
     else:
         name_new = [i + j[-1] for i in name for j in name if i[1:] == j[:-1]]
-    # gsp理论，
     return name_new
 
 
@@ -44,55 +43,26 @@ def gps_crdi(name, data):
     return r
 
 
-def gps2(data):
-    r_1_di = {}
+def gps(data):
+    r_di = {}
     for i in data:
         for j in set(i.split(',')):
-            if r_1_di.get(j) is None:
-                r_1_di[j] = 0
-            r_1_di[j] += 1
+            if r_di.get(j) is None:
+                r_di[j] = 0
+            r_di[j] += 1
 
-    di = gps_de(r_1_di, 2)  # 限制最小频数
+    di = gps_de(r_di, 2)
     nuu = 1
     ret = {}
     while True:
         print('L' + str(nuu) + ':', di)
         name_n = gps_cr(di, nuu == 1)
-        print(name_n)
         r_di = gps_crdi(name_n, data)
         di = gps_de(r_di, 2)
         if len(di) == 0:
             break
         ret.update(di)
         nuu += 1
-    return ret
-
-
-def gps(data):
-    r_1_di = {}
-    for i in data:
-        for j in set(i.split(',')):
-            if r_1_di.get(j) is None:
-                r_1_di[j] = 0
-            r_1_di[j] += 1  # 统计各个词的频次
-
-    di = gps_de(r_1_di, 2)  # 限制最小频数
-    print('L1:', di)
-    name = []
-    for k, v in di.items():
-        name.append(k)  # 取出需要的词
-    name_new = [i + j for i in name for j in name]  # 将取出来的词自由组合
-    r_di = gps_crdi(name_new, data)  # 计算各个自由组合出现的频次
-    di_3 = gps_de(r_di, 2)  # 将取出来的组合再次用频次限制
-    nuu = 1
-    ret = {}
-    while len(di_3) != 0:
-        print('L' + str(nuu + 1) + ':', di_3)
-        name_n = gps_cr(di_3, False)
-        r_di = gps_crdi(name_n, data)
-        di_3 = gps_de(r_di, 2)
-        nuu += 1
-        ret.update(di)
     return ret
 
 
@@ -118,8 +88,6 @@ def calc():
         print('--------------------------------------------')
         print(order)
         di_3 = gps(order)
-        di_3p = gps2(order)
-        print(di_3 == di_3p)
         if di_3 is not None and len(di_3) != 0:
             di_3s[k] = di_3
         print('--------------------------------------------')
